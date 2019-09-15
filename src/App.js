da885ef69ee6283b4328b73536004599e222f40c
Horbynko Ivan <horbynko.i@gmail.com>
2019-09-15 08:10:20 +0300

    added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import { Container} from 'react-bootstrap';
+import {Home} from "./pages/Home";
+import {About} from "./pages/About";
 
 function App() {
   return (
-    <div className="container">
+      <BrowserRouter>
+        <Container>
+          <Switch>
+            <Route path={'/'} exact component={Home}/>
+            <Route path={'/about'} exact component={About}/>
+          </Switch>
+        </Container>
 
-    </div>
+      </BrowserRouter>
   );
 }
 
