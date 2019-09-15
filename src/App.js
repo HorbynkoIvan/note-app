77f90d72643f45ef23c5c3c1571c1afd6c3dc461
Horbynko Ivan <horbynko.i@gmail.com>
2019-09-15 09:31:02 +0300

    added basic Alert

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Container} from 'react-bootstrap';
 import {Home} from "./pages/Home";
 import {About} from "./pages/About";
 import {NavBar} from "./components/NavBar";
+import {AlertComponent} from "./components/Alert";
 
 function App() {
   return (
       <BrowserRouter>
           <NavBar/>
         <div className="container pt-4">
+            <AlertComponent/>
           <Switch>
             <Route path={'/'} exact component={Home}/>
             <Route path={'/about'} exact component={About}/>
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,11 +1,11 @@
 import React from "react"
-import {ListGroup} from "react-bootstrap";
+import {ListGroup, CloseButton} from "react-bootstrap";
 
 export const Notes = ({notes}) => {
     return (
         <ListGroup>
             {notes.map((note)=>(
-                <ListGroup.Item key={note.id}>{note.title}</ListGroup.Item>
+                <ListGroup.Item key={note.id}>{note.title} <CloseButton/></ListGroup.Item>
             ))}
         </ListGroup>
     )
